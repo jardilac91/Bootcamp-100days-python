4ef3b16de297137e2204dff45bf4a4b3c830ade4
Jorge_Ardila <jardilac91@gmail.com>
2023-07-02 17:04:59 -0500

    Project 1 - Band Name generator

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to band name generator")
+city = input("Enter the city name:\n")
+pet_name = input("Enter the pet's name:\n")
+
+print("Your band name could be "+ city + " " + pet_name)
